0a750196992b8b6bfa36646cd07ac5f016f98876
mustafaer <mustafaerpro@gmail.com>
2022-12-14 03:38:00 +0300

    file formating

--- prisma/category.seed.ts
+++ prisma/category.seed.ts
@@ -2,12 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const categories = [
-  { name: 'Housing rent' },
-  { name: 'Electricity' },
-  { name: 'Water' },
-  { name: 'Air Conditioning' },
-];
+const categories = [{ name: 'Housing rent' }, { name: 'Electricity' }, { name: 'Water' }, { name: 'Air Conditioning' }];
 
 async function main() {
   await prisma.category.createMany({
@@ -19,7 +14,7 @@ main()
   .then(async () => {
     await prisma.$disconnect();
   })
-  .catch(async (e) => {
+  .catch(async e => {
     console.error(e);
     await prisma.$disconnect();
     process.exit(1);
